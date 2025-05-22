bb756a32cb04bd16b52144ad3aaff87aac6bd2de
prismzdrive <145449829+RadomirTikkerbar@users.noreply.github.com>
2025-05-22 13:32:19 +0300

    Add files via upload

---
+++ IApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace KooliProjekt.BlazorApp
+{
+    public interface IApiClient
+    {
+        Task<Result<Order>> Get(int id);
+        Task<Result<List<Order>>> List();
+        Task<Result> Save(Order list);
+        Task Delete(int id);
+    }
+
+    public class Order
+    {
+        [ExcludeFromCodeCoverage]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public string UserId { get; set; }
+
+        // mis tooted tellib klient
+        public IList<Order> Lines { get; set; }
+
+        public Order()
+        {
+            Lines = new List<Order>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,65 +1,20 @@
-using KooliProjekt.Data;
-using KooliProjekt.Services;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using KooliProjekt.BlazorApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace KooliProjekt
+namespace KooliProjekt.Blazor
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddScoped<ITodoListService, TodoListService>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
-#if DEBUG
-            using (var scope = app.Services.CreateScope())
-            using (var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>())
-            using (var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>())
-            { 
-                context.Database.EnsureCreated();
-                SeedData.Generate(context, userManager);
-            }
-#endif
-            app.Run();
+            await builder.Build().RunAsync();
         }
     }
 }
